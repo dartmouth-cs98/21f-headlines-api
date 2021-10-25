b47eeed44a9e09a1b91c18eaa1aa6dfca02a35aa
Julietelisa <juliet.elisa.giraso.22@dartmouth.edu>
2021-10-24 23:09:26 -0400

    post routes for articles and questions

--- src/controllers/article_controller.js
+++ src/controllers/article_controller.js
@@ -19,3 +19,34 @@ export const getArticle = (req, res) => {
       res.status(500).send({ message: 'Unable to get article' });
     });
 };
+
+export const addArticle = (articleInfo) => {
+  const article = new Article();
+  article.name = articleInfo.name; 
+  article.source = articleInfo.source; 
+  article.url = articleInfo.url; 
+  article.image_url = articleInfo.image_url; 
+  article.article_text= articleInfo.article_text; 
+  article.summary = articleInfo.summary; 
+  article.questions = articleInfo.questions; 
+  article.category = articleInfo.category; 
+
+  try {
+    const savedArticle = await article.save();
+    return savedArticle;
+  } catch (error) {
+    throw new Error(`unable to add article to database: ${error}`);
+  }
+}
+
+export const updateArticle = async (id, info) => {
+  try {
+    // await updating an article by id
+    const options = { new: true };
+    const article = await Article.findByIdAndUpdate(id, fields, options);
+    // return *updated* article object
+    return article;
+  } catch (error) {
+    throw new Error(`update error: ${error}`);
+  }
+};
--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import * as Articles from './controllers/article_controller';
+import * as Questions from './controllers/question_controller';
 
 const router = Router();
 
@@ -9,5 +10,28 @@ router.get('/', (req, res) => {
 
 router.route('/articles')
   .get(Articles.getArticles);
+
+router.route('/addArticle')
+  .post(async (req, res) => {
+    try {
+      const result = await Articles.addArticle(req.body);
+      res.json(result);
+    } catch (error) {
+      res.status(422).send({ error: error.toString() });
+    }
+});
+
+router.route(':articleID/addQuestion')
+  .post(async (req, res) => {
+    try {
+      const qn = await Questions.addQuestion(req.body);
+      const qns = qn.article_ref.questions;
+      qns.push(qn);
+      const article = await Articles.updateArticle(req.params.articleID, { events });
+      res.json(article);
+    } catch (error) {
+      res.status(422).send({ error: error.toString() });
+    }
+});
 
 export default router;
